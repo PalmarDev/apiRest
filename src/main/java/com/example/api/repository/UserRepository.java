02b96354c1c1c4b2efa2c9b3666aaf3ad907cdd7
Gabriel Palmar <palmarthc@gmail.com>
2023-10-20 11:35:29 -0400

    cambio en repository una letra mal puesta

--- src/main/java/com/example/api/repository/UserRepository.java
+++ src/main/java/com/example/api/repository/UserRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
-    User findByUsernameOrEmaiil(String username, String email);
 }
